bb61a1528c7c34f149e3142c462ff2ccf871652d
wf001 <wf001@diax.xyz>
2024-10-24 16:50:28 +0900

    add options

--- lua/runme.lua
+++ lua/runme.lua
@@ -15,7 +15,7 @@ local function terminal_stmt(opt)
   return ":terminal "
 end
 
-local function runit(commands)
+local function runit(commands, opt)
   if commands == nil then
     vim.notify("argument cant be empty", vim.log.levels.WARN)
   end
@@ -23,16 +23,28 @@ local function runit(commands)
   vim.cmd("autocmd TermOpen * setlocal nonumber")
   vim.cmd("autocmd TermOpen * setlocal norelativenumber")
 
-  vim.cmd(":split")
-  vim.cmd(terminal_stmt({}) .. matched_cmd(vim.fn.expand("%:e"), commands) .. "")
+  local pos = opt['position'] or 'belowright'
+  local dir = opt['direction'] and " vsplit" or " split"
+  local resize_dir = dir == "vsplit" and "vertical " or ""
+
+  vim.cmd(":" .. pos .. dir)
+
+  if opt["size"] ~= nil then
+    vim.cmd(":" .. resize_dir .. "resize " .. opt["size"])
+  end
+  vim.cmd(terminal_stmt(opt) .. matched_cmd(vim.fn.expand("%:e"), commands) .. "")
   vim.api.nvim_feedkeys("i", "n", false)
 end
 
 local set_key = vim.api.nvim_create_user_command
 
-function M.setup(commands)
+function M.setup(commands, opt)
   set_key("RunMe", function()
-    runit(commands)
+    runit(commands, opt or {})
+  end, {})
+  set_key("RunMeVertical", function()
+    opt.direction = true
+    runit(commands, opt or {})
   end, {})
 end
 
